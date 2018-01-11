45ad89fa98fc013e439d630af8bb492c3f17ea8f
Jaren Hamblin <jjjjaren@gmail.com>
2018-01-10 17:08:38 -0800

    Minor updates

--- Example/Pods/Target Support Files/Pods-TeslaKit_Example/Pods-TeslaKit_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-TeslaKit_Example/Pods-TeslaKit_Example-frameworks.sh
@@ -111,10 +111,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TeslaKit/TeslaKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TeslaKit/TeslaKit.framework"
 fi
